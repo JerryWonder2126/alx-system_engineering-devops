74311365695f5668de53fd7e4ee04e06869a431c
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-08 11:26:11 +0100

    fix: update puppet script to setup nginx server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,10 +12,8 @@ exec { 'Update apt-get' :
   command => '/usr/bin/env apt-get -y',
 }
 
--> file { 'Create backup of config' :
-  ensure => present,
-  source => '/etc/nginx/sites-available/default',
-  path   => '/etc/nginx/sites-available/default.bak'
+-> exec { 'Create backup of config' :
+  command => '/usr/bin/env cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak'
 }
 
 -> exec { 'Parse config and create backup' :
@@ -24,7 +22,7 @@ exec { 'Update apt-get' :
 
 -> file_line { 'add header' :
   ensure => present,
-  line   => "\n\tadd_header X-Served-By ${hostname};",
+  line   => "\n\tadd_header X-Served-By \$hostname;",
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
 }
@@ -34,7 +32,31 @@ exec { 'Update apt-get' :
   content => "Hello World!\n"
 }
 
+-> file_line { 'add 404 config' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\n\t# 404 configuration\n\terror_page 404 /custom_404.html;\
+            \n\tlocation /custom_404.html {\n\t\troot /var/www/html;\
+             \n\t\tinternal;\n\t}\n"
+}
+
+-> file { 'Create custom_404.html file' :
+  path    => '/var/www/html/custom_404.html',
+  content => "Ceci n'est pas une page\n"
+}
+
+
+-> file_line { 'add redirect config' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\n\t# Redirect configuration\n\tlocation /redirect_me \
+            {\n\t\trewrite /redirect_me https://youtube.com permanent;\n\t}\n"
+}
+
 -> service { 'Ensure nginx is running' :
-  ensure => running,
-  name   => 'nginx',
+  ensure     => running,
+  name       => 'nginx',
+  hasrestart => true
 }
