d7780bd4505c83bc95cd5ff0b97dfe1cc5e7ab05
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-08 10:00:08 +0100

    fix: update puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,40 @@
 # Setup Nginx server with a custom HTTP header
 
-exec { '/usr/bin/env apt-get -y update' : }
--> package { 'nginx':
-   ensure => installed,
+# Run updates
+exec { 'Update apt-get' :
+  command => '/usr/bin/env apt-get -y update'
+}
+
+# Install nginx
+-> package { 'Install nginx':
+  ensure  => installed,
+  name    => 'nginx',
+  command => '/usr/bin/env apt-get -y',
+}
+
+-> file { 'Create backup of config' :
+  ensure => present,
+  source => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-available/default.bak'
+}
+
+-> exec { 'Parse config and create backup' :
+  command => "/usr/bin/env awk '!/(\s)?#/' /etc/nginx/sites-available/default.bak > /etc/nginx/sites-available/default"
 }
 
 -> file_line { 'add header' :
-   ensure => present,
-   line => "\tadd_header X-Served-By ${hostname};",
-   path => '/etc/nginx/sites-available/default',
-   after => 'server_name _;,
+  ensure => present,
+  line   => "\n\tadd_header X-Served-By ${hostname};",
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
 }
 
--> file { '/var/www/html/index.html' :
-   content => 'Hello World!'
+-> file { 'Create index file' :
+  path    => '/var/www/html/index.html',
+  content => "Hello World!\n"
 }
 
--> service { 'nginx':
-   ensure => running,
+-> service { 'Ensure nginx is running' :
+  ensure => running,
+  name   => 'nginx',
 }
