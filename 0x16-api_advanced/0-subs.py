143e09d15641b9a9bfc83907d0fdba3d31c7f1db
Jerry Wonder <jerrycul2001@gmail.com>
2023-05-10 09:14:24 +0100

    fix: change logic implementation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ def number_of_subscribers(subreddit):
     Returns the number of subscriber to a subreddit
     Returns 0 if the subreddit isn't valid
     """
-    url = "https://www.redit.com/r/{}/about.json()".format(subreddit)
+    url = "https://redit.com/r/{}/about.json()".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    subscribers = response.json().get("data").get("subscribers")
-    return subscribers
+    if response.status_code == 200:
+        subscribers = response.json().get("data").get("subscribers")
+        return subscribers
+    return 0
