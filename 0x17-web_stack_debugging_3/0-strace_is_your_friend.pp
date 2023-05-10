332ccc2a91e77e694504134dcd87986226af7a21
Jerry Wonder <jerrycul2001@gmail.com>
2023-05-10 14:14:27 +0100

    fix: add fixes to puppet script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Change wrong extension .phpp to .php in wp-settings.php (wordpress config file)
 
 exec { 'fix wordpress config file':
-  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
-  path    => '/usr/bin/env'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/bin'
 }
